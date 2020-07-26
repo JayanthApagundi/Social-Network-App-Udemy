76fb3474c409080df7cf234b64028bfd68b5a1c8
Jayanth Apagundi <42775370+JayanthApagundi@users.noreply.github.com>
2020-07-27 01:02:58 +0530

    Delete Profile to be done

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -146,4 +146,72 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+// @route DELETE api/profile
+// @desc Delete profile, user, and posts related to the user
+// @access Private
+
+router.delete('/', auth, async (req, res) => {
+  try {
+    //to-do Remove posts related to user
+    //Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    //Remove User
+    await User.findOneAndRemove({ _id: req.user.id });
+    res.json({ msg: 'User Deleted' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error ');
+  }
+});
+
+// @route PUT/POST api/profile/experience
+// @desc Add profile experience
+// @access Private
+router.put(
+  '/experience',
+  [
+    auth,
+    [
+      check('title', 'Title is Required').not().isEmpty(),
+      check('company', 'Company is required').not().isEmpty(),
+      check('from', 'From date is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    //Destructing
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    } = req.body;
+
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      profile.experiences.unshift(newExp);
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 module.exports = router;
