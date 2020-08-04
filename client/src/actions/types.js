6f16b525d7935aa74de7ffd4bc5f9d83d1ce3095
Jayanth Apagundi <42775370+JayanthApagundi@users.noreply.github.com>
2020-08-04 21:50:00 +0530

    Redux Reducers to be done

---
+++ client/src/actions/types.js
@@ -0,0 +1,3 @@
+// Actions holds our HTTP requests and so on and types.js contains all our actions
+export const SET_ALERT = 'SET_ALERT';
+export const REMOVE_ALERT = 'REMOVE_ALERT';
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Landing = () => {
   return (
@@ -11,12 +12,12 @@ export const Landing = () => {
             other developers
           </p>
           <div className='buttons'>
-            <a href='register.html' class='btn btn-primary'>
+            <Link to='/register' class='btn btn-primary'>
               Sign Up
-            </a>
-            <a href='login.html' class='btn btn-light'>
+            </Link>
+            <Link to='/login' class='btn btn-light'>
               Login
-            </a>
+            </Link>
           </div>
         </div>
       </div>
