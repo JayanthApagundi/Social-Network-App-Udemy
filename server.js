d9e1e57e634e961ad649f47e053e4564d0936747
Jayanth Apagundi <42775370+JayanthApagundi@users.noreply.github.com>
2020-08-21 19:43:34 +0530

    Ready to deploy

--- server.js
+++ server.js
@@ -10,16 +10,13 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extened: false }));
 
-// Initial Check
-//app.get('/', (req, res) => res.send('API Running'));
-
 // Define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-// Serve static access in production
+// Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
