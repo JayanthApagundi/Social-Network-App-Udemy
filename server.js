5874f4149a257619c54373c2d166073c3fdb95d1
Jayanth Apagundi <42775370+JayanthApagundi@users.noreply.github.com>
2020-08-21 18:38:44 +0530

    Ready to Deploy

--- server.js
+++ server.js
@@ -1,24 +1,33 @@
-const express = require('express'); //requirement
+const express = require('express'); // requirement
+const connectDB = require('./config/db'); // getting the db.js file
+const path = require('path');
 
-const connectDB = require('./config/db'); //getting the db.js file
+const app = express(); // calling out express
 
-const app = express(); //calling out express
-
-//Connecting database
+// Connecting database
 connectDB();
 
-//Init Middleware
+// Init Middleware
 app.use(express.json({ extened: false }));
 
-//Initial Check
-app.get('/', (req, res) => res.send('API Running'));
+// Initial Check
+//app.get('/', (req, res) => res.send('API Running'));
 
-//Define routes
+// Define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-const PORT = process.env.PORT || 5000; //when we deploy to heroku this port number will be used.
+// Serve static access in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000; // when we deploy to heroku this port number will be used.
 
 app.listen(PORT, () => console.log(`server started at port ${PORT}`));
