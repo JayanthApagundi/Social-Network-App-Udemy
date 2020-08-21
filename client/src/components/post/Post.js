0851d8ed8f322e8ff72ad2d1764cdf717a0c1b67
Jayanth Apagundi <42775370+JayanthApagundi@users.noreply.github.com>
2020-08-21 18:18:21 +0530

    Deploy to be done

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -3,13 +3,32 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getPost } from '../../actions/post';
+import PostItem from '../posts/PostItem';
+import { Link } from 'react-router-dom';
+import CommentForm from './CommentForm';
+import CommentItem from './CommentItem';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
   useEffect(() => {
     getPost(match.params.id);
   }, [getPost]);
 
-  return <div>Post</div>;
+  return loading || post === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <Link to='/posts' className='btn btn-light'>
+        Back to Posts
+      </Link>
+      <PostItem post={post} showActions={false} />
+      <CommentForm postId={post._id} />
+      <div className='comments'>
+        {post.comments.map((comment) => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
+    </Fragment>
+  );
 };
 
 Post.propTypes = {
